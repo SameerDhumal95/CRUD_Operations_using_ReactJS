e9ffc5a1952bf9b422a375752cc4e25ee1d0c744
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2022-12-22 23:08:21 +0530

    CRUD

---
+++ src/components/Employees.js
@@ -0,0 +1,24 @@
+const Employees = [
+  {
+    id: "1",
+    Name: "Sameer",
+    Age: "26",
+  },
+  {
+    id: "2",
+    Name: "Shubham",
+    Age: "24",
+  },
+  {
+    id: "3",
+    Name: "Pritam",
+    Age: "26",
+  },
+  {
+    id: "4",
+    Name: "Ajeet",
+    Age: "27",
+  },
+];
+
+export default Employees;
